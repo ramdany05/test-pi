af5956dc136418fa23f03fdbb765d17384498cd6
unknown <suhandiramdany@gmail.com>
2024-06-03 04:48:07 +0700

    deleted: feed

--- src/index.js
+++ src/index.js
@@ -2,7 +2,6 @@
 const express = require('express');
 const app = express();
 const authRoutes = require('./routes/authRoutes');
-const feedRoutes = require('./routes/feedRoutes');
 const usersRoutes = require('./routes/usersRoutes');
 require('dotenv').config();
 const port = process.env.PORT;
@@ -11,7 +10,6 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 app.use('/api/auth', authRoutes);
-app.use('/api/feed', feedRoutes);
 app.use('/api/users', usersRoutes);
 
 app.listen(port, () => {
