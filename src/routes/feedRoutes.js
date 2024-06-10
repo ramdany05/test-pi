3c57a1f058561f1ece96783450194773e85ad04a
Radiit <radityaaydin.com>
2024-06-10 22:39:41 +0700

    fix: feedService authorization

--- src/routes/feedRoutes.js
+++ src/routes/feedRoutes.js
@@ -1,12 +1,13 @@
 const express = require('express');
 const authMiddleware = require('../middlewares/authMiddleware.js')
-const { getFeed, getFeedById, createFeed, deleteFeed } = require('../controllers/feedController');
+const { getFeed, getFeedById, createFeed, deleteFeed, updateFeed } = require('../controllers/feedController');
 
 const router = express.Router();
 
-router.get('/getAllFeed',authMiddleware, getFeed);
+router.get('/getAllFeed', getFeed);
 router.get('/:id', authMiddleware, getFeedById);
 router.post('/createFeed', authMiddleware, createFeed);
+router.put('/:id', authMiddleware, updateFeed)
 router.delete('/:id', authMiddleware, deleteFeed);
 
 
